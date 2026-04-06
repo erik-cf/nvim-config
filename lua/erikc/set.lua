4679c092d9538275e3733d05673e32b85cdaed3a
Erik Cabezuelo <erikc@oseainnovations.com>
2026-04-06 11:21:00 +0200

    feat: add tree-sitter

--- lua/erikc/set.lua
+++ lua/erikc/set.lua
@@ -8,9 +8,8 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
- 
 vim.opt.wrap = false
- 
 vim.opt.incsearch = true -- incremental search
- 
 vim.opt.termguicolors = true
+vim.diagnostic.config({ virtual_text = true })
+
