35f58283ad3bccc4352521a3e4fd8c1dd7aac99f
Erik Cabezuelo <erikc@oseainnovations.com>
2026-04-13 11:14:40 +0200

    feat: add lualine plugin

--- lua/erikc/lazy/init.lua
+++ lua/erikc/lazy/init.lua
@@ -4,4 +4,5 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live gr
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
 return {}
