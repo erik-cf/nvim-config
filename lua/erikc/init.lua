4037b7f4fde9336ee9ff9c2291035247381bf2ac
Erik Cabezuelo <erikc@oseainnovations.com>
2026-04-26 18:20:20 +0200

    feat: add diffview and lualine configs

--- lua/erikc/init.lua
+++ lua/erikc/init.lua
@@ -1,10 +1,15 @@
+vim.opt.termguicolors = true
+vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_transparent_background = 1
+vim.g.gruvbox_material_foreground = 'material'
 require("erikc.remap")
-require("erikc.init_lazy")
+require("erikc.config.lazy")
 require("erikc.set")
+vim.cmd('colorscheme gruvbox-material')
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
- 
+
 require'nvim-treesitter'.install { 'go', 'typescript' }
 
--- lua/erikc/lazy/colors.lua
+++ lua/erikc/lazy/colors.lua
@@ -1,3 +1,1 @@
-vim.api.nvim_set_hl(0, "Normal", { bg = "None" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "None" })
 return {}
