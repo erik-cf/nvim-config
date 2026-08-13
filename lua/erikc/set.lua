051aab7c74a914d1a8de177a47659db280bbda0b
Erik Cabezuelo <erikc@oseainnovations.com>
2026-08-13 17:41:52 +0200

    feat: add code actions on save for go, lua and ts

--- lua/erikc/set.lua
+++ lua/erikc/set.lua
@@ -12,4 +12,34 @@ vim.opt.wrap = false
 vim.opt.incsearch = true -- incremental search
 vim.opt.termguicolors = true
 vim.diagnostic.config({ virtual_text = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go", "*.ts", "*.lua" },
+  command = "lua vim.lsp.buf.format()"
+})
 
+local function organize_imports(bufnr, timeout_ms)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  timeout_ms = timeout_ms or 1000
+  local params = {
+    command = "_typescript.organizeImports",
+    arguments = { vim.api.nvim_buf_get_name(bufnr) },
+  }
+  vim.lsp.buf_request_sync(bufnr, "workspace/executeCommand", params, timeout_ms)
+end
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.go", "*.ts" },
+  callback = function(ev)
+    organize_imports(ev.buf, 1000)
+  end
+})
+
+-- lspconfig.tsserver.setup {
+--   on_attach = on_attach,
+--   capabilities = capabilities,
+--   commands = {
+--     OrganizeImports = {
+--       organize_imports,
+--       description = "Organize Imports"
+--     }
+--   }
+-- }
